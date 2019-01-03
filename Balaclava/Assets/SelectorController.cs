0194d58be54d7c01763d9106ee368ace17f5bb55
jetese <jetese2@gmail.com>
2019-01-03 20:01:05 +0100

    Objeto cambia material a verde cuando lo tienes a rango

---
+++ Balaclava/Assets/SelectorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorController : MonoBehaviour
+{
+    public Material iluminated;
+    Material oldMaterial;
+    bool selectable = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SelectableObject")
+        {
+            // Change the cube color to green.
+            oldMaterial = other.gameObject.GetComponent<MeshRenderer>().material;
+            other.gameObject.GetComponent<MeshRenderer>().material = iluminated;
+            selectable = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "SelectableObject")
+        {
+            // Revert the cube color to white.
+            other.gameObject.GetComponent<MeshRenderer>().material = oldMaterial;
+            selectable = false;
+        }
+    }
+}
