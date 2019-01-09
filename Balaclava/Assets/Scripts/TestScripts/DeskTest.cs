dbfed89db750ef2846cc21a8a1cc578efb5f66d7
sergio_sanchez <sergete86@gmail.com>
2019-01-10 00:55:14 +0100

    Joysticks arreglados

---
+++ Balaclava/Assets/Scripts/TestScripts/DeskTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskTest : MonoBehaviour
+{    
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("OpenDrawer");
+        StartCoroutine(waitToClose());
+    }
+
+    IEnumerator waitToClose()
+    {
+        yield return new WaitForSeconds(3);
+        anim.SetTrigger("CloseDrawer");
+    }
+}
