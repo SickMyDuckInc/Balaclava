b7867a59bbb270a0c59ec4dc4c0c1d4921f75e1e
sergio_sanchez <sergete86@gmail.com>
2019-01-10 16:35:44 +0100

    fusion con Pepe escena4

--- Balaclava/Assets/Scripts/Spawner/SpawnerPlayer.cs
+++ Balaclava/Assets/Scripts/Spawner/SpawnerPlayer.cs
@@ -21,7 +21,7 @@ public class SpawnerPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*string operatingSystem = SystemInfo.operatingSystem;
+        string operatingSystem = SystemInfo.operatingSystem;
 
         if (operatingSystem.ToLower().Contains(DEFAULT_OPERATING_SYSTEM_ANDROID) || operatingSystem.ToLower().Contains(DEFAULT_OPERATING_SYSTEM_APPLE))
         {
@@ -38,15 +38,15 @@ public class SpawnerPlayer : MonoBehaviour
             WindowsPlayerPrefab.SetActive(true);
             Destroy(MobilePlayerPrefab);
             ISDEVICE = false;
-        }*/
+        }
 
         //Hide always at start
         ActionButton.SetActive(false);
         HelpText.SetActive(false);
 
-        MobilePlayerPrefab.SetActive(true);
+        /*MobilePlayerPrefab.SetActive(true);
         Destroy(WindowsPlayerPrefab);
-        ISDEVICE = true;
+        ISDEVICE = true;*/
     }
 
 }
