ff56791304ebc1df04e5f3f61405f930303ef690
jetese <jetese2@gmail.com>
2019-01-16 19:56:47 +0100

    Proyecto balaclava version 2

---
+++ Balaclavav2/Balaclava/Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
+
+public class SceneController : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void OpenUrlJs(string str);
+    public void NextSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void NextSceneById(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    public void LoadUrl(string url)
+    {
+        OpenUrlJs(url);
+    }
+}
